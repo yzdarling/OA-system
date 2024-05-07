6d3dff8558c714f1032abd5e3ed0a17506ae462d
xiaoyuan <3023492630@qq.com>
2024-05-07 17:30:05 +0800

    feat:完成员工分析页面开发

--- src/pages/Dashborad/index.jsx
+++ src/pages/Dashborad/index.jsx
@@ -1,3 +1,55 @@
+import { useModel } from '@umijs/max';
+import { useEffect } from 'react';
+
+import DashTatal from './components/DashTotal';
+import DashSeniority from './components/DashSeniority';
+import DashAgeAndMarriage from './components/DashAgeAndMarriage';
+import DashOldStaffAndStar from './components/DashOldStaffAndStar';
+import DashAgeAndNumber from './components/DashAgeAndNumber';
+import DashEducationAndGender from './components/DashEducationAndGender';
+
 export default function DashBorad() {
-  return <div>DashBorad</div>;
+  const {
+    wordingYearsMaps,
+    educationPie,
+    genderPie,
+    total,
+    amountDataList,
+    ageMap,
+    marriageList,
+    departmentList,
+    constellationList,
+    initDashboradList,
+  } = useModel('dashborad');
+
+  useEffect(() => {
+    initDashboradList();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <DashTatal total={total} />
+      </div>
+      <div>
+        <DashSeniority amountDataList={amountDataList} />
+      </div>
+      <div>
+        <DashEducationAndGender genderPie={genderPie} educationPie={educationPie} total={total} />
+      </div>
+      <div>
+        <DashAgeAndMarriage genderPie={genderPie} marriageList={marriageList} total={total} />
+      </div>
+      <div>
+        <DashAgeAndNumber ageMap={ageMap} departmentList={departmentList} />
+      </div>
+      <div>
+        <DashOldStaffAndStar
+          wordingYearsMaps={wordingYearsMaps}
+          constellationList={constellationList}
+          total={total}
+        />
+      </div>
+    </div>
+  );
 }
